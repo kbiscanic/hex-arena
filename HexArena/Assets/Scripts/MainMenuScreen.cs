7cacfd586e5bb28f8ddf8085c1909ef692b0c803
Kemichar <luka.hrabar@gmail.com>
2015-12-18 18:31:58 +0100

    Podjela na scene

---
+++ HexArena/Assets/Scripts/MainMenuScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScreen : MonoBehaviour {
+
+	public string gameSceneName = "GameScene";
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void newGame(){
+		Application.LoadLevel(gameSceneName);
+	}
+
+	public void quitGame(){
+		Application.Quit();
+	}
+}
