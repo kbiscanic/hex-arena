7d2ae64b96aba0935fd4ec6f246c8e0087083626
Kemichar <luka.hrabar@gmail.com>
2015-12-18 19:35:03 +0100

    Initial platform setup

---
+++ HexArena/Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScreen : MonoBehaviour {
+
+	public string mainMenuSceneName = "MainMenuScene";
+	float screenDuration=3.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		screenDuration -= Time.deltaTime;
+		if (screenDuration <= 0 || Input.anyKey)
+			Application.LoadLevel (mainMenuSceneName);
+	}
+}
