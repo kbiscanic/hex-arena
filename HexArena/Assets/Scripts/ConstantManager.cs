aca5d0be8de424d5752ec329140c1edd4a725725
Kemichar <luka.hrabar@gmail.com>
2015-12-19 00:04:52 +0100

    Disappearing platforms

---
+++ HexArena/Assets/Scripts/ConstantManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantManager : MonoBehaviour {
+
+	public const string playerTag = "Player";
+	public const string projectileTag = "Projectile";
+
+	public const float platformDeathTimer = 5.0f;
+	public const float platformFadeTimer = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- HexArena/Assets/Scripts/HexPlatform.cs
+++ HexArena/Assets/Scripts/HexPlatform.cs
@@ -3,13 +3,67 @@ using System.Collections;
 
 public class HexPlatform : MonoBehaviour {
 
+	enum State {
+		Alive, Dying, Dead, Reviving
+	}
+
+	State state = State.Alive;
+	float deathTimer = 0;
+	float fadeTimer = 0;
+
+	private Renderer rend;
+
 	// Use this for initialization
 	void Start () {
-	
+		rend = this.GetComponentInChildren<Renderer> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (state == State.Dying) {
+			fadeTimer -= Time.deltaTime;
+
+			Color tmpColor = rend.material.color;
+			tmpColor.a = fadeTimer / ConstantManager.platformFadeTimer;
+			rend.material.SetColor ("_Color", tmpColor);
+
+			if (fadeTimer <= 0) {
+				state = State.Dead;
+				deathTimer = ConstantManager.platformDeathTimer;
+			}
+		} else if (state == State.Reviving) {
+			fadeTimer -= Time.deltaTime;
+
+			Color tmpColor = rend.material.color;
+			tmpColor.a = 1 - fadeTimer / ConstantManager.platformFadeTimer;
+			rend.material.SetColor ("_Color", tmpColor);
+
+			if (fadeTimer <= 0) {
+				state = State.Alive;
+				this.GetComponentInChildren<Collider> ().enabled = true;
+			}
+		} else if (state == State.Dead) {
+			deathTimer -= Time.deltaTime;
+
+			if (deathTimer <= 0) {
+				state = State.Reviving;
+				fadeTimer = ConstantManager.platformFadeTimer;
+			}
+		}
 	}
+
+	void OnCollisionEnter(Collision col){
+		Transform otherObject = col.transform;
+
+		if (otherObject.tag == ConstantManager.playerTag)
+			return;
+		
+		if (otherObject.tag == ConstantManager.projectileTag && state == State.Alive) {
+			// example; can depend on projectile type or whatever else
+			state = State.Dying;
+			fadeTimer = ConstantManager.platformFadeTimer;
+			this.GetComponentInChildren<Collider> ().enabled = false;
+		}
+	}
+
 }
