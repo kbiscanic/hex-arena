4bc71da4597506cdfbf37dad658ebe6cff422f8d
Kristijan Biscanic <kbiscanic@gmail.com>
2015-12-20 21:44:23 +0100

    add basic action bar; add skill icon assets

---
+++ HexArena/Assets/Scripts/Actions/Action.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Action : MonoBehaviour
+{
+
+	public float cooldown = 0f;
+	[HideInInspector]
+	public float currentCooldown = 0f;
+	public string description;
+	public Image cdIcon;
+	public KeyCode key;
+
+	public void Execute ()
+	{
+		if (currentCooldown <= 10e-6) {
+			// nesto poput player.actions[desc].exec() ?
+			Debug.Log (description + " cast.");
+
+			currentCooldown = cooldown;
+		} else {
+			Debug.Log (description + " on cooldown.");
+		}
+	}
+
+	void Update ()
+	{
+		if (currentCooldown > 0 && cooldown > 0) {
+			currentCooldown -= Time.deltaTime;
+			cdIcon.fillAmount = 1 - currentCooldown / cooldown;
+		}
+	}
+}
---
+++ HexArena/Assets/Scripts/Actions/ActionBar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ActionBar : MonoBehaviour {
+
+	public List<Action> actions;
+
+	void Update () {
+		foreach(Action action in actions){
+			if (Input.GetKeyDown (action.key)) {
+					action.Execute ();
+			}
+		}
+	}
+}
